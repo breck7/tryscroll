bdfd8bc6b5900774ab3e671e0d759131d684d0e4
Breck Yunits <breck@Brecks-MacBook-Pro.local>
2024-05-02 14:09:25 -0400

    checkpoint

--- dist/app.js
+++ dist/app.js
@@ -530,71 +530,83 @@ const DEFAULT_PROGRAM = `title This is Scroll. The keyword for title is title.
 # Scroll is an extensible alternative to Markdown.
  https://scroll.pub Scroll
 
+***
+
+// You can have multiple columns
+startColumns 2
+
 ## Links are different
-You put links _after_ the text, like this one.
+You put links _after_ the text, like this one. The code:
  https://scroll.pub one.
 aboveAsCode
 
-## It has footnotes^note
+? What's the benefit for using today?
+- A very flat plain text format
+- Keeps your thoughts and data organized
+- Ready to _grow_ with _you_.
+- Designed to last.
+
+# Scroll supports tables
+Language	NodeTypes
+HTML	~142
+Markdown	~192
+Scroll	~174 + yours
+
+# Images in Scroll
+image https://scroll.pub/blog/screenshot.png
+ caption This is a screenshot of a blog
+  https://breckyunits.com/ blog
+aboveAsCode
+
+## Scroll has footnotes^note
 
 ^note Sometimes called endnotes
 
-? What's the benefit for using today?
-A simple plain text format that keeps your thoughts and data clean that is ready to _grow with you_.
-
-? What might this become?
-Who knows. Perhaps a large ontology of types of thought?
-
-# A Table of Node Types
-Format  Types
-HTML   ~142
-Markdown  ~192
-Scroll  1,000's?
-
-gazetteCss`
+gazetteCss
+`
 
 class BrowserGlue extends AbstractTreeComponentParser {
-  async fetchAndLoadScrollCodeFromUrlCommand(url) {
-    const code = await this.fetchText(url)
-    return code
-  }
+	async fetchAndLoadScrollCodeFromUrlCommand(url) {
+		const code = await this.fetchText(url)
+		return code
+	}
 
-  async fetchText(url) {
-    const result = await fetch(url)
-    const text = await result.text()
-    return text
-  }
+	async fetchText(url) {
+		const result = await fetch(url)
+		const text = await result.text()
+		return text
+	}
 
-  getFromLocalStorage() {
-    return localStorage.getItem(LocalStorageKeys.scroll)
-  }
+	getFromLocalStorage() {
+		return localStorage.getItem(LocalStorageKeys.scroll)
+	}
 
-  async fetchCode() {
-    const hash = this.willowBrowser.getHash().substr(1)
-    const deepLink = new TreeNode(decodeURIComponent(hash))
-    const fromUrl = deepLink.get(UrlKeys.url)
-    const code = deepLink.getNode(UrlKeys.scroll)
+	async fetchCode() {
+		const hash = this.willowBrowser.getHash().substr(1)
+		const deepLink = new TreeNode(decodeURIComponent(hash))
+		const fromUrl = deepLink.get(UrlKeys.url)
+		const code = deepLink.getNode(UrlKeys.scroll)
 
-    // Clear hash
-    history.pushState("", document.title, window.location.pathname)
+		// Clear hash
+		history.pushState("", document.title, window.location.pathname)
 
-    if (fromUrl) return this.fetchAndLoadScrollCodeFromUrlCommand(fromUrl)
-    if (code) return code.childrenToString()
+		if (fromUrl) return this.fetchAndLoadScrollCodeFromUrlCommand(fromUrl)
+		if (code) return code.childrenToString()
 
-    const localStorageCode = this.getFromLocalStorage()
-    if (localStorageCode) return localStorageCode
+		const localStorageCode = this.getFromLocalStorage()
+		if (localStorageCode) return localStorageCode
 
-    return DEFAULT_PROGRAM
-  }
+		return DEFAULT_PROGRAM
+	}
 
-  async init(grammarCode) {
-    window.scrollParser = new HandGrammarProgram(grammarCode).compileAndReturnRootParser()
-    const scrollCode = await this.fetchCode()
+	async init(grammarCode) {
+		window.scrollParser = new HandGrammarProgram(grammarCode).compileAndReturnRootParser()
+		const scrollCode = await this.fetchCode()
 
-    window.app = EditorApp.setupApp(scrollCode, window.innerWidth, window.innerHeight)
-    window.app.start()
-    return window.app
-  }
+		window.app = EditorApp.setupApp(scrollCode, window.innerWidth, window.innerHeight)
+		window.app.start()
+		return window.app
+	}
 }
 
 window.BrowserGlue = BrowserGlue
