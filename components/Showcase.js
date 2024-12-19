83c9bc44db6bb9004be1c95855d0ef8eee5be86c
Breck Yunits <breck7@gmail.com>
2024-12-19 14:23:13 -0800

    Preserve scroll position

--- components/Showcase.js
+++ components/Showcase.js
@@ -2,6 +2,12 @@ const { AbstractParticleComponentParser } = require("scrollsdk/products/Particle
 
 class ShowcaseComponent extends AbstractParticleComponentParser {
   async refresh() {
+    // Store the current scroll position of the iframe
+    const iframe = document.getElementById("theIframe")
+    const scrollTop = iframe.contentWindow ? iframe.contentWindow.scrollY : 0
+    const scrollLeft = iframe.contentWindow ? iframe.contentWindow.scrollX : 0
+
+    // Perform the refresh
     await this.root.buildMainProgram()
     const { mainOutput } = this.root
     let content = mainOutput.content
@@ -9,7 +15,19 @@ class ShowcaseComponent extends AbstractParticleComponentParser {
       content = `<pre>${content}</pre>`
     }
 
-    document.getElementById("theIframe").srcdoc = content
+    // Add script to restore scroll position after load
+    const scrollScript = `
+      <script>
+        window.addEventListener('load', function() {
+          window.scrollTo(${scrollLeft}, ${scrollTop});
+        });
+      </script>
+    `
+
+    // Inject the content with the scroll restoration script
+    document.getElementById("theIframe").srcdoc = content + scrollScript
+
+    // Set up link click prevention
     jQuery("#theIframe")
       .contents()
       .find("a")
